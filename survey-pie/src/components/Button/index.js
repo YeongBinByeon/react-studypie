f8b59464063869c135ec74dc20e650167b5dba94
yeongbin <yeongbin6483@gmail.com>
2024-08-18 22:49:31 +0900

    섹션 11 / 11-3. 스타일 변수 만들기

---
+++ survey-pie/src/components/Button/index.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+import { PRIMARY, SECONDARY, TERTIARY } from '../../constants/color';
+
+const colorMap = {
+  PRIMARY, // === PRIMAR: PRIMAR
+  SECONDARY,
+  TERTIARY,
+};
+
+const Button = styled.button`
+  padding: 16px 24px;
+  width: 200px;
+  color: ${(props) => {
+    return colorMap[props.type].BUTTON.DEFAULT.COLOR;
+  }};
+  background: ${({ type }) => colorMap[type].BUTTON.DEFAULT.BACKGROUND};
+  border-radius: 4px;
+  font-weight: bold;
+
+  border: ${({ type }) =>
+    type === 'TERTIARY'
+      ? `1px solid ${TERTIARY.BUTTON.DEFAULT.BORDER}`
+      : 'none'};
+
+  &:hover {
+    color: ${(props) => {
+      return colorMap[props.type].BUTTON.HOVER.COLOR;
+    }};
+    background: ${({ type }) => colorMap[type].BUTTON.HOVER.BACKGROUND};
+  }
+
+  &:active {
+    color: ${(props) => {
+      return colorMap[props.type].BUTTON.PRESSED.COLOR;
+    }};
+    background: ${({ type }) => colorMap[type].BUTTON.PRESSED.BACKGROUND};
+  }
+
+  &:disabled {
+    color: ${(props) => {
+      return colorMap[props.type].BUTTON.DISABLED.COLOR;
+    }};
+    background: ${({ type }) => colorMap[type].BUTTON.DISABLED.BACKGROUND};
+  }
+`;
+
+export default Button;
