76a8c2973e6d395499c854d5a076c2755ecbbebc
yeongbin <yeongbin6483@gmail.com>
2024-10-08 09:45:11 +0900

    7-2. 기본 컴포넌트 구현하기

--- memo/src/App.js
+++ memo/src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import "./App.css";
+import MemoContainer from "./components/MemoContainer";
+import SideBar from "./components/SideBar";
+import SideBarFooter from "./components/SideBarFooter";
 
 function App() {
-  return <div className="App">App</div>;
+  const [memos, setMemos] = useState([
+    {
+      title: "Memo 1",
+      content: "This is memo 1",
+      createdAt: 1728346675314, // new Date().getTime();
+      updatedAt: 1728346675314,
+    },
+    {
+      title: "Memo 2",
+      content: "This is memo 2",
+      createdAt: 1728346675312,
+      updatedAt: 1728346675312,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <SideBar memos={memos} />
+      <MemoContainer />
+      <SideBarFooter />
+    </div>
+  );
 }
 
 export default App;
